1b7cb3edf9a4fdb42ca54ceb9bdc0b727659efb0
Mayra <143430528+mayyzenacs@users.noreply.github.com>
2025-11-22 18:40:17 -0300

    update style and icons

--- script.js
+++ script.js
@@ -4,11 +4,14 @@ const obs = new IntersectionObserver((entries) => {
 
         if (entry.isIntersecting) {
             entry.target.classList.add('show-element')
+            // obs.unobserve(entry.target);
+        } else {
+            entry.target.classList.remove('show-element')
         }
     });
 });
 
-const hidden = document.querySelectorAll('.hidden-left, .hidden.right')
+const hidden = document.querySelectorAll('.hidden-left, .hidden-right')
 hidden.forEach((element) => {
     obs.observe(element)    
 } )
