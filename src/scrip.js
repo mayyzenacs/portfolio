a3b852e8b4545d2033d812dc46c47c6314575827
Mayra <143430528+mayyzenacs@users.noreply.github.com>
2025-12-14 19:10:04 -0300

    fix mobile responsiveness

--- src/scrip.js
+++ src/scrip.js
@@ -1,17 +1,16 @@
 export function animatedAbout() {
+  const obs = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      console.log(entry);
 
-    const obs = new IntersectionObserver((entries) => {
-        entries.forEach((entry) => {
-            console.log(entry)
-    
-            if (entry.isIntersecting) {
-                entry.target.classList.add('show-element')
-                // obs.unobserve(entry.target);
-            } else {
-                entry.target.classList.remove('show-element')
-            }
-        });
+      if (entry.isIntersecting) {
+        entry.target.classList.add("show-element");
+        // obs.unobserve(entry.target);
+      } else {
+        entry.target.classList.remove("show-element");
+      }
     });
-    const hidden = document.querySelectorAll('.hidden-left, .hidden-right')
-    hidden.forEach((element) => obs.observe(element))
+  });
+  const hidden = document.querySelectorAll(".hidden-left, .hidden-right");
+  hidden.forEach((element) => obs.observe(element));
 }
